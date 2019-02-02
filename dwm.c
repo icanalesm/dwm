0e93879d8e4b16cd4649fa8a3fc6b65a1dce1558
Isaac Andrés Canales Martínez <icanalesm0500@gmail.com>
2019-02-02 10:33:37 +0100

    Update size of tag indicator

--- dwm.c
+++ dwm.c
@@ -701,7 +701,7 @@ drawbar(Monitor *m)
 {
 	int x, w, sw = 0;
 	int boxs = drw->fonts->h / 9;
-	int boxw = drw->fonts->h / 6 + 2;
+	int boxw = drw->fonts->h / 6 + 1;
 	unsigned int i, occ = 0, urg = 0;
 	Client *c;
 
