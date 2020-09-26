3f17f89777ff9b5e0c8596a46bc7e1b7fb7cb462
Isaac Andrés Canales Martínez <icanalesm0500@gmail.com>
2020-09-26 11:51:34 +0200

    Make the key binding for st execute /bin/sh

--- config.h
+++ config.h
@@ -62,7 +62,7 @@ static const char *dmenucmd[] = {
 	"-nf", CLR_NORFG, "-sb", CLR_SELBG, "-sf", CLR_SELFG, NULL
 };
 static const char *cmd_term[] = {
-	"st", NULL
+	"st", "-e", "/bin/sh", NULL
 };
 static const char *cmd_status[] = {
 	"tstat", "set", NULL
