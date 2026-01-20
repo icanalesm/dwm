c0d824677ae79142afd34901a165c845311c30c0
Isaac Andrés Canales Martínez <icanalesm0500@gmail.com>
2026-01-20 10:32:09 +0400

    Add colour schemes for the different elements in the bar
    Revert de970550241c28841e9b4da316f2eb78aa0e6455 (i.e., remove titlecolor
    patch and apply a custom version of the colorbar patch
    (https://dwm.suckless.org/patches/colorbar).
    
    Colour schemes now work as follows:
    - SchemeNorm and SchemeSel are used to colour the window title in the
      bar when showtitle == 1. When showtitle == 0, SchemeNorm is used to
      colour the part of the bar where the window title would appear.
    - SchemeNorm and SchemeSel are used to colour the border of windows.
    - SchemeTagNorm and SchemeTagSel are used to colour the tags.
    - SchemeLayout and SchemeStatus are used to colour, respectively, the
      layout indicator and the status.
    - The third colour in SchemeTagNorm, SchemeTagSel, SchemeLayout and
      SchemeStatus is not used, but must be specified.

--- config.def.h
+++ config.def.h
@@ -15,10 +15,13 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
-	/*                fg         bg         border   */
-	[SchemeNorm]  = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]   = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeTitle] = { col_gray4, col_cyan,  col_cyan  },
+	/*                  fg         bg         border   */
+	[SchemeNorm]    = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]     = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeTagNorm] = { col_gray3, col_gray1, "#000000" },
+	[SchemeTagSel]  = { col_gray4, col_cyan,  "#000000" },
+	[SchemeLayout]  = { col_gray3, col_gray1, "#000000" },
+	[SchemeStatus]  = { col_gray3, col_gray1, "#000000" },
 };
 
 /* tagging */
